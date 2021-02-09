a2304f07c8cd0303fa094af31a2c5622fa00201c
ANDRI YABU NDAPA REHING <andriyabu@gmail.com>
2021-02-09 18:52:13 +0700

    add filter for users to view index page and welcome message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :find_message, only: [:edit, :show, :update, :destroy]
-
+  # before_action :authenticate_user!
   def index
     @messages = Message.all.order(created_at: :desc)
   end
